0d18b5ea64ad38dd72b4a6c3d11c52b10001cfda
chunsiyang <chunsi.yang@gmail.com>
2019-11-11 13:14:47 +0800

    增加服务端有码番号格式化能力

--- app/formatter/censoredFormatter.py
+++ app/formatter/censoredFormatter.py
@@ -1,9 +1,12 @@
 from app.formatter.basicFormatter import BasicFormater
 
+
 class CensoredFormatter(BasicFormater):
 
     def format(code):
         hCode = str(code).upper()
+        if hCode[-4] == '0' and hCode[-5] == '0':
+            hCode = hCode[0:-5] + hCode[-3:]
         if hCode[-4] != "-":
             if hCode[-4] == " ":
                 return hCode.replace(" ", "-")
