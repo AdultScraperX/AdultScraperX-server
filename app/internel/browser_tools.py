e86c6d5278d79d939853fcffdeba299e6a3395c5
chunsi.yang@gmail.com <chunsi.yang@gmail.com>
2019-12-09 23:20:35 +0800

    docker配置

--- app/internel/browser_tools.py
+++ app/internel/browser_tools.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import config as CONFIG
 
@@ -9,12 +7,17 @@ class BrowserTools:
     display = None
 
     def getBrowser(self):
-        firefox_opt = Options()
-        firefox_opt.headless = True
+
 
         if CONFIG.BROWSER_DRIVE is 'firefox':
+            from selenium.webdriver.firefox.options import Options
+            firefox_opt = Options()
+            firefox_opt.headless = True
             self.browser = webdriver.Firefox(options=firefox_opt)
         if CONFIG.BROWSER_DRIVE is 'chrome':
+            from selenium.webdriver.chrome.options import Options
+            firefox_opt = Options()
+            firefox_opt.headless = True
             self.browser = webdriver.Chrome(options=firefox_opt)
 
         return self.browser
