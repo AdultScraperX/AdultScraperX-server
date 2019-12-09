1d6240f3ac6beb25833f2f623a0acd08479ed182
chunsi.yang@gmail.com <chunsi.yang@gmail.com>
2019-12-09 23:22:09 +0800

    docker配置

--- app/internel/browser_tools.py
+++ app/internel/browser_tools.py
@@ -15,10 +15,11 @@ class BrowserTools:
             firefox_opt.headless = True
             self.browser = webdriver.Firefox(options=firefox_opt)
         if CONFIG.BROWSER_DRIVE is 'chrome':
-            from selenium.webdriver.chrome.options import Options
-            firefox_opt = Options()
-            firefox_opt.headless = True
-            self.browser = webdriver.Chrome(options=firefox_opt)
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+            self.browser = webdriver.Chrome(chrome_options=chrome_options)
 
         return self.browser
 
