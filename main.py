d1090d59fd22e58f0d8aad045c04f367218c5f36
chunsi.yang@gmail.com <chunsi.yang@gmail.com>
2019-12-10 12:46:17 +0800

    docker配置

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask import Flask, request
 from flask import render_template
 from flask import send_file
 
-from app.config import spider_config
+from config import spider_config
 
 if sys.version.find('2', 0, 1) == 0:
     try:
